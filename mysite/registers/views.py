5f162b67996a504c2c9000ffe4234de9c2e6ff9d
cynriquelme <cynthiariquelme86@gmail.com>
2021-06-01 09:01:43 -0400

    main

--- mysite/registers/views.py
+++ mysite/registers/views.py
@@ -1,6 +1,5 @@
 import django
-from django.forms import forms
-from .models import Register
+from .models import Category, Register
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
@@ -8,6 +7,7 @@ from django.shortcuts import redirect
 from django.contrib.admin.views.decorators import staff_member_required
 from django.utils.decorators import method_decorator
 from django.urls import reverse, reverse_lazy
+from django import forms
 
 # Create your views here.
 class RegisterListView(ListView):
@@ -22,6 +22,12 @@ class RegisterCreate(CreateView):
     fields = ['sub_category', 'description', 'status']
     success_url = reverse_lazy('registers:registers')
 
+    def get_form(self, form_class=None):
+        form = super(RegisterCreate, self).get_form()
+        # Modificar en tiempo reald
+        form.fields['description'].widget = forms.TextInput(attrs={'class':'form-control mb-2', 'placeholder':'Ingrese una descripción'})
+        return form
+
 @method_decorator(staff_member_required, name='dispatch')
 class RegisterUpdate(UpdateView):
     model = Register
@@ -35,3 +41,5 @@ class RegisterUpdate(UpdateView):
 class RegisterDelete(DeleteView):
     model = Register
     success_url = reverse_lazy('registers:registers')
+
+    
