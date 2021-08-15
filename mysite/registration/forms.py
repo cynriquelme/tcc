8028c13754adf99c93b7595d68e515c260779a08
cynriquelme <cynthiariquelme86@gmail.com>
2021-08-15 19:59:48 -0400

    tcc-07

--- mysite/registration/forms.py
+++ mysite/registration/forms.py
@@ -24,8 +24,8 @@ class ProfileForm(forms.ModelForm):
         fields = ['avatar', 'bio', 'link']
         widgets = {
             'avatar': forms.ClearableFileInput(attrs={'class':'form-control-file mt-3'}),
-            'bio': forms.Textarea(attrs={'class':'form-control mt-3','rows':3, 'placeholder':'Biografía'}),
-            'link': forms.URLInput(attrs={'class':'form-control mt-3', 'placeholder':'Enlace'}),
+            #'bio': forms.Textarea(attrs={'class':'form-control mt-3','rows':3, 'placeholder':'Biografía'}),
+            #'link': forms.URLInput(attrs={'class':'form-control mt-3', 'placeholder':'Enlace'}),
         }
 
 class EmailForm(forms.ModelForm):
